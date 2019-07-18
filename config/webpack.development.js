95840f19f751d107200337d70a14739ae3810b46
Jorge Blas Muñoz Huércano <jorge.munoz.huercano@gmail.com>
2019-07-18 20:29:43 +0200

    Updated development conf

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = () => ({
     output:{
-        filename: 'js/[name]-bundle.js',
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: ''
+        contentBase: '../src',
+	    historyApiFallback: true,
+        overlay: true, // Shows error overlay
+        inline: true,
+        port: 3000
     },
     devServer: {
         contentBase: 'dist',
