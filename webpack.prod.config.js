250c8f3d51a38b9b99829e97bdf1b28bbb6ea4ae
JBMunoz <jorge.munoz.huercano@gmail.com>
2021-11-08 12:47:24 +0100

    Added autprefixer to project

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -19,7 +19,12 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
 			},
 			{
 				test: /\.js$/,
